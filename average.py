4e8153e11f174f206db4f4e260a30fa8f7990914
aronton <github2310928>
2024-07-07 00:29:20 +0800

    240707

--- average.py
+++ average.py
@@ -3,78 +3,6 @@ import os
 import datetime
 import scriptCreator
 
-task = input("What is the task? (a)submit, (b)resubmit, (c)cancel Jobs: : \n")
-print(task)
-
-parameterlist = {"Spin":None,"L":{"L1":None,"L2":None,"dL":None},"J":{"J1":None,"J2":None,"dJ":None},\
-                 "D":{"D1":None,"D2":None,"dD":None},"seed":{"s1":None,"s2":None,"ds":None},\
-                 "BC":None,"Pdis":None,"bondDim":None,"dx":None,"check_Or_Not":None}
-Ncore = input("Ncore : \n")
-partition = input("partition : \n")
-print("key in parameter in the following format : \n\
-ex : Spin, L1, L2, delta_L, J1, J2, delta_J, D1, D2, delta_D, Pdis, bondDim, initialSampleNumber, finalSampleNumber, sampleDelta, check_Or_Not\n\
-ex : 15(Spin) 64(L) 1.1(J) 0.1(D) 10(Pdis) 40(bondDim) 1(initialSampleNumber) 20(finalSampleNumber) 5(sampleDelta), Y(check_Or_Not)\n")
-
-try:
-    parameterlist["Spin"]=input("Spin : ")
-
-    parameterlist["L"]["L1"]=input("L1 : ")
-    parameterlist["L"]["L2"]=input("L2 : ")
-    parameterlist["L"]["dL"]=input("dL : ")    
-
-    parameterlist["J"]["J1"]=input("J1 : ")
-    parameterlist["J"]["J2"]=input("J2 : ")
-    parameterlist["J"]["dJ"]=input("dJ : ")
-
-    parameterlist["D"]["D1"]=input("D1 : ")
-    parameterlist["D"]["D2"]=input("D2 : ")
-    parameterlist["D"]["dD"]=input("dD : ")
-
-    parameterlist["seed"]["s1"]=int(input("initialSampleNumber : "))
-    parameterlist["seed"]["s2"]=int(input("finalSampleNumber : "))
-    parameterlist["seed"]["ds"]=int(input("sampleDelta : "))
-    parameterlist["BC"] = input("BC : ")
-    parameterlist["Pdis"] = int(input("Pdis : "))
-    parameterlist["bondDim"] = int(input("bondDim : "))
-    parameterlist["dx"] = int(input("dx : "))
-    parameterlist["check_Or_Not"]=input("check_Or_Not(Y/N) : ")
-except KeyboardInterrupt:
-    print("shut down")
-    exit()
-
-
-
-# parameterlist["Spin"]=sys.argv[0]
-
-# parameterlist["L"]["L1"]=sys.argv[1]
-# parameterlist["L"]["L2"]=sys.argv[2]
-# parameterlist["L"]["dL"]=sys.argv[3]
-
-# parameterlist["J"]["J1"]=sys.argv[4]
-# parameterlist["J"]["J2"]=sys.argv[5]
-# parameterlist["J"]["dJ"]=sys.argv[6]
-
-# parameterlist["D"]["D1"]=sys.argv[7]
-# parameterlist["D"]["D2"]=sys.argv[8]
-# parameterlist["D"]["dD"]=sys.argv[9]
-
-# parameterlist["seed"]["s1"]=int(sys.argv[10])
-# parameterlist["seed"]["s2"]=int(sys.argv[11])
-# parameterlist["seed"]["ds"]=int(sys.argv[12])
-# parameterlist["BC"] = sys.argv[13]
-# parameterlist["Pdis"] = int(sys.argv[14])
-# parameterlist["bondDim"] = int(sys.argv[15])
-# parameterlist["dx"] = int(sys.argv[16])
-# parameterlist["check_Or_Not"]=sys.argv[17]
-
-
-
-print(parameterlist,"\n")
-for s in parameterlist:
-    print(s," : ",parameterlist[s])
-
-tSDRG_path="/home/aronton/tSDRG_random"
-
 def submut(parameterlist, Ncore, partition, tSDRG_path):
 
     p = parameterlist
@@ -199,12 +127,6 @@ def submut(parameterlist, Ncore, partition, tSDRG_path):
                     script_name = jobName + "_" + now_date + "_" + now_time
                     script_path = script_path + "/" + script_name + "_random.sh"
                     output_path = output_path + "/" + script_name + "_random.out"
-                    
-                    # print("jobName:",jobName)
-                    # print("script_name:",script_name)
-                    # print("script_path:",script_path)
-                    # print("output_path:",output_path)
-                    
                     context = template.copy()
                     with open(script_path, "w") as file:
                         context[1] = context[1].replace("replace1", "scopion" + str(partition))
@@ -219,23 +141,26 @@ def submut(parameterlist, Ncore, partition, tSDRG_path):
                     submit_cmd = " ".join(submit_cmd_list)
                     os.system(submit_cmd)
 
-def show(parameterlist, Ncore, partition):
-    
-    print("show")
-    
+def find(parameterlist, Ncore, partition):
+    print("find")
     p = parameterlist
+    # flag = input("Job_state R/P")
+    if p["status"] == "R":
+        Job_state = "RUNNING"
+    elif p["status"] == "P":
+        Job_state = "PENDING"
+    elif p["status"] == "" :
+        Job_state = ""
+        
     
     job_list = os.popen("squeue -u aronton " + "-p scopion" + str(partition) + " -o \"%%.12i %%.12P %%.90j %%.8T\"")
 
-    a = list(job_list)
+    job_list = list(job_list)
 
-    del a[0]
-    # print(a)
-    for i in range(len(a)):
-        a[i] = a[i].split()    
-    for i in range(len(a)):
-        print(a[i])
-    # print(a)
+    del job_list[0]
+    for i in range(len(job_list)):
+        job_list[i] = job_list[i].split()    
+
 
     if (p["L"]["L1"] != "") and (p["L"]["L2"] != "") and (p["L"]["dL"] != ""): 
         L=scriptCreator.paraList("L",p["L"])
@@ -245,10 +170,16 @@ def show(parameterlist, Ncore, partition):
         L_p_str = L.p_str_list
         L_s100 = L.s100_list
         L_p_s100 = L.p_s100_list
-        print(L_str)
-        b = []
-        for i in L_str:
-            b = b + list(filter(lambda n: i in n[2],a))
+        
+        temp = []
+        for l in L_str:
+            for e in job_list:
+                if l in e[2]:
+                    print(l)
+                    print(e[2])
+                    temp.append(e)
+        job_list = temp
+
             
     if (p["J"]["J1"] != "") and (p["J"]["J2"] != "") and (p["J"]["dJ"] != ""): 
         J=scriptCreator.paraList("Jdis",p["J"])
@@ -259,10 +190,14 @@ def show(parameterlist, Ncore, partition):
         J_s100 = J.s100_list
         J_p_s100 = J.p_s100_list
 
-        print(J_str)
-        c = []
-        for i in J_str:
-            c = c + list(filter(lambda n: i in n[2],b))
+        temp = []
+        for j in J_str:
+            for e in job_list:
+                if j in e[2]:
+                    temp.append(e)
+        job_list = temp
+
+
 
 
     if (p["D"]["D1"] != "") and (p["D"]["D2"] != "") and (p["D"]["dD"] != ""): 
@@ -273,11 +208,21 @@ def show(parameterlist, Ncore, partition):
         D_p_str = D.p_str_list
         D_s100 = D.s100_list
         D_p_s100 = D.p_s100_list
+
+        temp = []
+        for d in D_str:
+            for e in job_list:
+                if d in e[2]:
+                    temp.append(e)
+            # if i in job_list
+        job_list = temp
         
-        print(D_str)
-        d = []
-        for i in D_str:
-            d = d + list(filter(lambda n: i in n[2],c))
+        # print(D_str)
+        # # d = []
+        # for i in D_str:
+        #     job_list = job_list + list(filter(lambda n: i in n[2],job_list))
+    # else:
+    #     d = c
             
     s1 = p["seed"]["s1"]
     s2 = p["seed"]["s2"]
@@ -287,115 +232,101 @@ def show(parameterlist, Ncore, partition):
     BC=p["BC"]
     check_Or_Not=p["check_Or_Not"]
     
-    # print("ddd")
-    # for i in range(len(d)):
-    #     print(d[i])    
         
     if (p["Spin"] != ""): 
         Spin=p["Spin"]
-        e = list(filter(lambda n: Spin in n[2],d))
+        job_list = list(filter(lambda n: Spin in n[2],job_list))
     if (p["Pdis"] != ""): 
         Pdis=p["Pdis"]
-        f = list(filter(lambda n: Pdis in n[2],e))        
+        job_list = list(filter(lambda n: Pdis in n[2],job_list))        
     if (p["bondDim"] != ""): 
         bondDim=p["bondDim"]
-        g = list(filter(lambda n: bondDim in n[2],f))   
+        job_list = list(filter(lambda n: bondDim in n[2],job_list))   
     if (p["BC"] != ""): 
         BC=p["BC"]
-        h = list(filter(lambda n: BC in n[2],g))     
-    # print("hhh")
-    # for i in range(len(h)):
-    #     print(h[i])
-        
+        job_list = list(filter(lambda n: BC in n[2],job_list))    
+    if (Job_state != ""): 
+        job_list = list(filter(lambda n: Job_state in n[3],job_list))     
+     
+    return job_list
 
-# def cancel(parameterlist, Ncore, partition):
+def cancel(parameterlist, Ncore, partition):
+
+    job_list = find(parameterlist, Ncore, partition)
+
+    print("Cancel : \n\n")
+    print("------------------------------------------------- \n\n")
+
+    for i in range(len(job_list)):
+        cmd = "scancel " + job_list[i][0]
+        print(cmd + " : " + job_list[i][2])    
+        os.system(cmd)
     
-#     p = parameterlist
-
-#     L=scriptCreator.paraList("L",parameterlist["L"])
-#     L_num = L.num_list
-#     L_s100 = L.s100_list
-#     L_str = L.str_list
-#     L_p_num = L.p_num_list
-
-#     print("L_num:",L_num)
-#     print("L_p_num:",L_p_num)
-#     print("L_str:",L_str)
-#     print("L_p_str:",L_p_str)
-#     print("L_s100:",L_s100)
-#     print("L_p_s100:",L_p_s100)
-
-#     S=scriptCreator.Spara("Seed",parameterlist["seed"])
-#     S_num = S.toS()
-#     S_str = S.toStr()
-#     s1 = parameterlist["seed"]["s1"]
-#     s2 = parameterlist["seed"]["s2"]
-
-#     J=scriptCreator.paraList("Jdis",parameterlist["J"])
-#     J_num = J.num_list
-#     J_p_num = J.p_num_list
-#     J_str = J.str_list
-#     J_p_str = J.p_str_list
-#     J_s100 = J.s100_list
-#     J_p_s100 = J.p_s100_list
-
-#     D=scriptCreator.paraList("Dim",parameterlist["D"])
-#     D_num = D.num_list
-#     D_p_num = D.p_num_list
-#     D_str = D.str_list
-#     D_p_str = D.p_str_list
-#     D_s100 = D.s100_list
-#     D_p_s100 = D.p_s100_list
-
-#     Spin=parameterlist["Spin"]
-#     Pdis=parameterlist["Pdis"]
-#     dx=parameterlist["dx"]
-#     bondDim=parameterlist["bondDim"]
-#     BC=parameterlist["BC"]
+def show(parameterlist, Ncore, partition):
     
-#     job_list = os.popen("squeue -u aronton " + "-p scopion" + str(partition) + " -o \"%%.12i %%.12P %%.90j %%.8T\"")
-#     a = list(job_list)
-#     del a[0]
-#     for i in range(len(a)):
-#         a[i] = a[i].split()
-#     print(a)
-#     print(J_str)
-#     b = []
-#     for i in J_str:
-#         b = b + list(filter(lambda n: i in n[2],a))
-#     # print(list(b))
-#     print(D_str)
-#     c = []
-#     for i in D_str:
-#         c = c + list(filter(lambda n: i in n[2],b))
-#     # print(list(c))
-#     print(Pdis)
-#     d = []
-#     d = d + list(filter(lambda n: str(Pdis) in n[2],c))
-#     # print(list(d))
+    
+    job_list = find(parameterlist, Ncore, partition)
 
-#     e = []
-#     for i in L_str:
-#         e = e + list(filter(lambda n: i in n[2],d))    
-#     e=list(e)
-       
-#     return 0
+    print("show\n\n")
+    print("------------------------------------------------------\n\n")
 
-# def p_del(b, title, L_s100):
-#     for l_i,l in enumerate(L_s100):
-#         b=filter(lambda s : title + str(l) in s[2],b)
-#     return b
-# def s_del(b, S_num):
-#     for s_i in range(len(S_num)):
-#         seed = S_num[s_i]
-#         b=filter(lambda s : "seed1=" + str(seed[0]) in s[2],b)
-#         b=filter(lambda s : "seed2=" + str(seed[-1]) in s[2],b)
-#     return b
+    for i in range(len(job_list)):
+        print(job_list[i])
+    
+
+
+task = input("What is the task? (a)submit, (b)resubmit, (c)cancel Jobs: : \n")
+print(task)
+
+parameterlist = {"Spin":None,"L":{"L1":None,"L2":None,"dL":None},"J":{"J1":None,"J2":None,"dJ":None},\
+                 "D":{"D1":None,"D2":None,"dD":None},"seed":{"s1":None,"s2":None,"ds":None},\
+                 "BC":None,"Pdis":None,"bondDim":None,"dx":None,"check_Or_Not":None,"status":None}
+Ncore = input("Ncore : \n")
+partition = input("partition : \n")
+print("key in parameter in the following format : \n\
+ex : Spin, L1, L2, delta_L, J1, J2, delta_J, D1, D2, delta_D, Pdis, bondDim, initialSampleNumber, finalSampleNumber, sampleDelta, check_Or_Not\n\
+ex : 15(Spin) 64(L) 1.1(J) 0.1(D) 10(Pdis) 40(bondDim) 1(initialSampleNumber) 20(finalSampleNumber) 5(sampleDelta), Y(check_Or_Not)\n")
+
+try:
+    parameterlist["Spin"]=input("Spin : ")
+
+    parameterlist["L"]["L1"]=input("L1 : ")
+    parameterlist["L"]["L2"]=input("L2 : ")
+    parameterlist["L"]["dL"]=input("dL : ")    
+
+    parameterlist["J"]["J1"]=input("J1 : ")
+    parameterlist["J"]["J2"]=input("J2 : ")
+    parameterlist["J"]["dJ"]=input("dJ : ")
+
+    parameterlist["D"]["D1"]=input("D1 : ")
+    parameterlist["D"]["D2"]=input("D2 : ")
+    parameterlist["D"]["dD"]=input("dD : ")
+
+    parameterlist["seed"]["s1"]=input("initialSampleNumber : ")
+    parameterlist["seed"]["s2"]=input("finalSampleNumber : ")
+    parameterlist["seed"]["ds"]=input("sampleDelta : ")
+    parameterlist["BC"] = input("BC : ")
+    parameterlist["Pdis"] = input("Pdis : ")
+    parameterlist["bondDim"] = input("bondDim : ")
+    parameterlist["dx"] = input("dx : ")
+    parameterlist["check_Or_Not"]=input("check_Or_Not(Y/N) : ")
+    parameterlist["status"]=input("R/P) : ")
+
+except KeyboardInterrupt:
+    print("shut down")
+    exit()
+
+
+print(parameterlist,"\n")
+for s in parameterlist:
+    print(s," : ",parameterlist[s])
+
+tSDRG_path="/home/aronton/tSDRG_random"
+
+
 if task == "a":
-    print(task)
     submut(parameterlist, Ncore, partition, tSDRG_path)
 elif task == "b":
-    print(task)
     show(parameterlist, Ncore, partition)
-else:
-    pass
+elif task == "c":
+    cancel(parameterlist, Ncore, partition)
---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH --partition=replace1
+#SBATCH --ntasks=1
+#SBATCH --job-name=replace2
+#SBATCH --cpus-per-task=replace3
+#SBATCH --output=replace4
+
+date
+
+Spin=${1}
+
+tSDRG_path=${12}
+
+output_path=${13}
+
+echo -e "current dir:"
+cd $tSDRG_path"/tSDRG/Main_"$Spin
+
+pwd
+
+echo "script_directory"
+echo "$(output_path)"
+
+L=${2}
+
+J=${3}
+
+D=${4}
+
+BC=${5}
+
+bonDim=${6}
+
+Pdis=${7}
+
+s1=${8}
+
+s2=${9}
+
+CheckOrNot=${10}
+
+Ncore=${11}
+
+echo "Spin         ==> $Spin"
+echo "L         ==> $L"
+echo "J         ==> $J"
+echo "D         ==> $D"
+echo "BC        ==> $BC"
+echo "bonDim        ==> $bonDim"
+echo "Pdis        ==> $Pdis"
+echo "s1        ==> $s1"
+echo "s2        ==> $s2"
+echo "CheckOrNot        ==> $CheckOrNot"
+echo "Ncore        ==> $Ncore"
+echo "tSDRG_path        ==> $tSDRG_path"
+echo "output_path        ==> $output_path"
+
+$tSDRG_path/tSDRG/Main_${Spin}/Spin${Spin}_gaussian.exe $L $bonDim $Pdis $J $D $BC $s1 $s2 $CheckOrNot
+echo "tSDRG_path/tSDRG/Main/Spin${Spin}_gaussian.exe $L $bonDim $Pdis $J $D $BC $s1 $s2 $CheckOrNot"
+
+date
